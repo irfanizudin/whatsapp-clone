513cdc8dda404785de68297b8395562aa82eebe1
irfanizudin <irfanizzudyn@gmail.com>
2023-03-25 10:53:14 +0700

    Initial Commit

---
+++ chat-app/chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_appApp.swift
+//  chat-app
+//
+//  Created by Irfan Izudin on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
